5827f9393a70a256c7adae52702010982e021120
Dino1984WB <williamkbukowkidev@gmail.com>
2023-06-16 22:29:46 -0400

    finally got the window to stay open..

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Author: William Bukowski, using chatgpt to make this pong game with a gui in python 3
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Ellipse
@@ -18,9 +20,11 @@ class Paddle(Widget):
 
 
 class Ball(Widget):
-    def __init__(self, **kwargs):
+    def __init__(self, paddle, opponent, **kwargs):
         super(Ball, self).__init__(**kwargs)
-        self.vel = (choice([-1, 1]), choice([-1, 1]))  # Random initial velocity
+        self.vel = [choice([-1, 1]), choice([-1, 1])]  # Random initial velocity
+        self.paddle = paddle
+        self.opponent = opponent
 
     def move(self):
         self.pos[0] += self.vel[0] * 4
@@ -31,16 +35,16 @@ class Ball(Widget):
             self.vel[1] *= -1
 
         # Bounce off paddles
-        if self.collide_widget(paddle) or self.collide_widget(opponent):
+        if self.collide_widget(self.paddle) or self.collide_widget(self.opponent):
             self.vel[0] *= -1
 
 
 class PongGame(Widget):
     def __init__(self, **kwargs):
         super(PongGame, self).__init__(**kwargs)
-        self.ball = Ball(pos=(Window.width/2, Window.height/2))
-        self.paddle = Paddle(pos=(20, Window.height/2-40))
-        self.opponent = Paddle(pos=(Window.width-40, Window.height/2-40))
+        self.paddle = Paddle(pos=[20, Window.height/2-40])
+        self.opponent = Paddle(pos=[Window.width-40, Window.height/2-40])
+        self.ball = Ball(paddle=self.paddle, opponent=self.opponent, pos=[Window.width/2, Window.height/2])
 
         self.add_widget(self.ball)
         self.add_widget(self.paddle)
@@ -54,4 +58,35 @@ class PongGame(Widget):
         self.ball.move()
 
         if "w" in self.keys_pressed:
-            self
+            self.paddle.move_up()
+        if "s" in self.keys_pressed:
+            self.paddle.move_down()
+
+        if self.ball.x > self.width - 50:
+            self.opponent.move_up()
+        if self.ball.x < 50:
+            self.opponent.move_down()
+
+        self.check_win()
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 2:
+            self.paddle.center_y = touch.y
+
+    def check_win(self):
+        if self.ball.x < -50:
+            print("You win!")
+            self.ball.vel = [0, 0]
+        if self.ball.x > self.width + 50:
+            print("You lose!")
+            self.ball.vel = [0, 0]
+
+
+class PongApp(App):
+    def build(self):
+        game = PongGame()
+        return game
+
+
+if __name__ == '__main__':
+    PongApp().run()
